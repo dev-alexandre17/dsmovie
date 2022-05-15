be0e363a8609a0dc7c147f7c805e60278ebd4bc8
dev-alexandre17 <alexandretec77@gmail.com>
2022-05-14 22:09:46 -0700

    Pagination

--- frontend/src/components/Pagination/index.tsx
+++ frontend/src/components/Pagination/index.tsx
@@ -1,16 +1,24 @@
-import {ReactComponent as Arrow} from 'assets/img/arrow.svg';
+import { ReactComponent as Arrow } from 'assets/img/arrow.svg';
+import { PixelPage } from 'types/pixel';
 import './styles.css';
 
-function Pagination() {
+type Props = {
+    page: PixelPage;
+    onChange: Function;
+}
+
+function Pagination({ page, onChange }: Props) {
 
     return (
         <div className="dspixel-pagination-container">
             <div className="dspixel-pagination-box">
-                <button className="dspixel-pagination-button" disabled={true} >
+                <button className="dspixel-pagination-button" disabled={page.first}
+                    onClick={() => onChange(page.number - 1)}>
                     <Arrow />
                 </button>
-                <p>{`${1} de ${3}`}</p>
-                <button className="dspixel-pagination-button" disabled={false} >
+                <p>{`${page.number + 1} de ${page.totalPages}`}</p>
+                <button className="dspixel-pagination-button" disabled={page.last} 
+                    onClick={() => onChange(page.number + 1)}>
                     <Arrow className="dspixel-flip-horizontal" />
                 </button>
             </div>
